8846ea6c6aa6c3d4bbcb8ac3725fbb9935afa236
yama0322 <55902857+yama0322@users.noreply.github.com>
2020-01-06 17:13:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,17 +5,21 @@ int main(int argc, char *argv[])
     int str[5];
     int i;
     
-    while(fgets(str, sizeof(str), stdin)){
-	    
 	i=0;
+	
+    while(1){
 	    
         if (atr[i] < 0 && str[-str[i] - 1] >= 0) {
-			printf("%d\n", str[-str[i] - 1]);
-		}
+		printf("%d\n", str[-str[i] - 1]);
+	}
 
-		if (str[i] < 0 && str[-str[i] - 1] < 0) {
-			printf("%d\n", str[-str[i]]);
-		}
+	else if(str[i] < 0 && str[-str[i] - 1] < 0) {
+		printf("%d\n", str[-str[i]]);
+	}
+	    
+	    else{
+	    break;
+	    }
 	    
     }
 
